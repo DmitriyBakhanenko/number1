e7632b24916e1a332b5f8ae64da1596b23021be9
DmitriyBakhanenko <bahanenko@gmail.com>
2020-12-20 15:58:19 +0200

    chng html clean & css->scss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from './history';
 
-import Header from './components/header/Header';
+//import Header from './components/header/Header';
 import Main from './components/main/Main';
 import About from './components/About';
 import Contacts from './components/Contacts';
@@ -10,7 +10,6 @@ import Contacts from './components/Contacts';
 const App = () => {
   return (
     <Router history={history}>
-      <Header />
       <Switch>
         <Route path='/' exact component={Main}></Route>
         <Route path='/about' exact component={About}></Route>
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import image from '../../assets/homepage02.jpg';
-import './Main.css';
+import './Main.scss';
 
 const Main = () => {
   return (
