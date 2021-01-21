0acc87c96e6b2a30973beaa56ab54319123460e3
DmitriyBakhanenko <bahanenko@gmail.com>
2021-01-21 22:16:02 +0200

    add styling

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import Collections from '../../components/collections/Collections';
-import './homepage.scss';
 
 const HomePage = () => {
   return (
     <React.Fragment>
-      <div className='linier_background' />
       <Collections />
     </React.Fragment>
   );
